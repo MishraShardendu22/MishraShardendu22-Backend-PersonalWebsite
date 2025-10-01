fd0ba0dba4290ddc73066166815443af7101ef17
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-10-01 23:42:50 +0530

    add: fixed kanban style updation fucntion
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

--- route/project.route.go
+++ route/project.route.go
@@ -10,14 +10,15 @@ func SetupProjectRoutes(app *fiber.App, secret string) {
 	// Public routes - no authentication required
 	app.Get("/api/projects", controller.GetProjects)
 	app.Get("/api/projects/kanban", controller.GetProjectsKanban)
-	app.Get("/api/projects/:id", controller.GetProjectByID)
 	// app.Get("/api/UpdateProjectOrderInitial",controller.UpdateProjectOrder)
-
+	
 	// Admin routes - authentication required
 	app.Post("/api/projects", middleware.JWTMiddleware(secret), controller.AddProjects)
+	app.Post("/api/projects/updateOrder", middleware.JWTMiddleware(secret), controller.UpdateProjectOrderKanban)
+
+	app.Get("/api/projects/:id", controller.GetProjectByID)
 	app.Put("/api/projects/:id", middleware.JWTMiddleware(secret), controller.UpdateProjects)
 	app.Delete("/api/projects/:id", middleware.JWTMiddleware(secret), controller.RemoveProjects)
-	app.Post("/api/projects/updateOrder", middleware.JWTMiddleware(secret), controller.UpdateProjectOrderKanban)
 }
 
 /*
